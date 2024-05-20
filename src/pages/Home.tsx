e0a461546ef8e105769500597a90b488087d99ec
mudasir <96320211+mud-ali@users.noreply.github.com>
2024-05-20 10:46:31 -0400

    add template stuff

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,8 +2,11 @@ import Header from "@/components/Header";
 
 export default function Home() {
   return (
-    <>
-        <Header></Header>
-    </>
+    <div className="flex flex-row">
+        <Header/>
+        <div className="pl-4 pt-8">
+            <h1 className="text-2xl font-bold">Welcome, Mudasir</h1>
+        </div>
+    </div>
   )
 }
