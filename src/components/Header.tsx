cd51ae31ae02cf3b69c3b8b4eb63853069f8ada1
mudasir <96320211+mud-ali@users.noreply.github.com>
2024-05-20 10:37:34 -0400

    create header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,46 @@
+import '@/css/header.css'
+import { Tooltip } from 'react-tooltip'
+import { faChartSimple, faChartColumn, faQuestion, faTable } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Link } from 'react-router-dom';
+
 export default function Header() {
     return (
-        <div>
-            <ul>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
+        <div className='header bg-slate-700 py-14 h-screen w-32'>
+            <ul className='list-none flex justify-between flex-col text-right h-full align-right'>
+                <li>
+                    <Link to="/grades" className='grades-link'>
+                        <FontAwesomeIcon icon={faChartSimple} />
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/history" className='history-link'>
+                        <FontAwesomeIcon icon={faChartColumn}/>
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/schedule" className='sched-gen-link'>
+                        <FontAwesomeIcon icon={faTable}/>
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/about" className='about-link'>
+                        <FontAwesomeIcon icon={faQuestion}/>
+                    </Link>
+                </li>
             </ul>
+            <Tooltip anchorSelect='.history-link' place="right">
+                Grade History
+            </Tooltip>
+            <Tooltip anchorSelect='.sched-gen-link' place="right">
+                Generate a Schedule
+            </Tooltip>
+            <Tooltip anchorSelect='.about-link' place="right">
+                About
+            </Tooltip>
+            <Tooltip anchorSelect='.grades-link' place="right">
+                View Your Grades
+            </Tooltip>
         </div>
     );
 }
