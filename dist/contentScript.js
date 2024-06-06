b319b87d122de425c3233f4b00f06cedc0c32250
JChang <jinchang223@gmail.com>
2024-06-06 13:51:46 -0400

    add two lines u figure out where

--- dist/contentScript.js
+++ dist/contentScript.js
@@ -6,10 +6,13 @@
 
 function scrapeGrades(){
     const classesData = document.querySelectorAll('tr[id^="ccid"]');
+    let info = {}
     for (let i = 0; i < classesData.length; i++) {
         let cName = classesData[i].querySelector('td[class="table-element-text-align-start"]').innerHTML.split("&nbsp;")[0].trim();
         let cGrades = Array.from(classesData[i].querySelectorAll('a[class="bold"]')).map(grade => grade.innerHTML.split("<br>"));
+        info[cName] = cGrades;
     }
+    localStorage.setItem("class_grades", info);
 }
 
 if (
