3d4e18c29abbbc3d001ec741ded11bef06cf610d
mudasir <96320211+mud-ali@users.noreply.github.com>
2024-06-06 13:42:57 -0400

    begin work on content script

--- dist/contentScript.js
+++ dist/contentScript.js
@@ -1,18 +1,21 @@
-const cheerio = require("cheerio");
-const axios = require("axios");
+// ==UserScript==
+// @name        Powerschool +
+// @description webscraper for powerschool
+// @match       https://bcts.powerschool.com/*
+// ==/UserScript==
 
-async function webScrape(){
-    const axiosResponse = await axios.request({
-        method: "GET",
-        url: "https://bcts.powerschool.com/guardian/home.html?_userTypeHint=student",
-        headers: {
-            "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
-        }
-    })
-    const $ = cheerio.load(axiosResponse.data);
+function scrapeGrades(){
+    const classesData = document.querySelectorAll('tr[id^="ccid"]');
+    for (let i = 0; i < classesData.length; i++) {
+        let cName = classesData[i].querySelector('td[class="table-element-text-align-start"]').innerHTML.split("&nbsp;")[0].trim();
+        let cGrades = Array.from(classesData[i].querySelectorAll('a[class="bold"]')).map(grade => grade.innerHTML.split("<br>"));
+    }
+}
 
-    const grades = [];
-    const classes = [];
-
+if (
+  document.URL.split("?")[0].startsWith(
+    "https://bcts.powerschool.com/guardian/home.html",
+  )
+) {
 
 }
