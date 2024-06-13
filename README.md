e2cfe27602c44ee05807e52b23d05f40697b8d3f
mudasir <96320211+mud-ali@users.noreply.github.com>
2024-06-13 02:16:54 -0400

    remove website

--- README.md
+++
@@ -1,5 +0,0 @@
-# Powerschool+
-
-An extension made to enhance the powerschool experience at BCA
-
-Mudasir, Aarav, Jin
--- main.js
+++
@@ -1,8 +0,0 @@
-import "./style.css";
-import viteLogo from "/vite.svg";
-
-document.querySelector("#app").innerHTML += `
-  <div>
-    <div class="text-3xl center text-black"></div>
-  </div>
-`;
